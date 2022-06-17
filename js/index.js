e4d1224a754fd765771ae312a4ba1e2fd7dcc2fd
NQ31 <1563602991@qq.com>
2022-06-17 03:03:53 +0000

    xxx

--- js/index.js
+++ js/index.js
@@ -14,8 +14,7 @@ node:"cors"
             <div class="remd_t1_item_txt">
                 <p>${item.name}</p>
             </div>
-        </div>
- `;
+        </div>`;
     });
    
 })
@@ -50,3 +49,63 @@ fetch("http://localhost:3000/personalized/newsong?limit=30",{
     });
    
 }))
+//热歌榜
+fetch("http://localhost:3000/personalized/newsong?limit=30",{
+    method:"GET",
+    node:"cors"
+}).then(r=>r.json().then(data=>{
+    console.log(data.result);
+    
+    data.result.forEach(item=>{
+        let author=""
+        //获取作者
+        item.song.artists.forEach(d=>{
+            author+=d.name+"/"
+        })
+        author=author.slice(0,author.length-1);
+        $(".new_song_list").innerHTML+=`
+        <a href="./play.html?id=${item.id}">
+        <div class="new_song_item">
+                   <div class="new_song_item_left">
+                       <div class="new_song_name">
+                        ${item.name}
+                       </div>
+                       <div class="new_song_author">
+                        ${item.song.sqMusic==null?author+"-"+item.name:"<div class='sqmusic'></div>"+author+"-"+item.name}
+                       </div>
+                   </div>
+                   <div class="new_song_item_right"></div>
+                   </div>
+                </a>`;
+    });
+   
+}))
+// 搜索功能
+$(".search").oninput=function(){
+    if($("search").value==null){
+        $(".default").style.display="block"
+        $(".search_remd").style.display="none"
+        $(".search_result").style.display="none"
+    }else{
+        $(".search").onkeyup=function(e){
+            if(e.key=="Enter"){
+             $(".default").style.display="none"
+             $(".search_remd").style.display="none"
+             $(".search_result").style.display="block" 
+             fetch(`http://localhost:3000/cloudsearch?keywords=${$(#search").value}`,{
+                method:"GET",
+                node:"cors"
+            }).then(r=>r.json().then(data=>{
+                console.log(data.result.song)
+                data.resulu.songs.forEach(element => {
+                    $(".search_redult").innerHTML+=`<a href=".play.html?id=${element.id}"<a><p>${element.name}</p></a>`
+                });
+            })
+        }else{
+            $(".default").style.display="none"
+            $(".search_remd").style.display="block"
+            $(".search_result").style.display="none"
+         }
+        }
+    }
+}
--- js/play.js
+++ js/play.js
@@ -7,13 +7,43 @@ fetch(`http://localhost:3000/song/url?id=${id}`,{
     console.log(data.data[0])
     $(".audio").innerHTML=`<audio src="${data.data[0].url}" controls autoplay></audio>`
 })
-// 播放效果
-$(".disk").onclick=function(){
-    if($(".audio audio").paused){
-        $(".audio audio").play()
-        $(".disk").classList.add("disk_rotate")//动起来
-    }else{
-        $(".audio audio").pause()
-        $(".disk").classList.remove("disk_rotate")//不动
+
+//获取歌曲详情
+fetch(`http://localhost:3000/song/detail?ids=${id}`,{
+    method:"GET",
+    node:"cors"
+}).then(r=>r.json()).then(data=>{
+    console.log(data.songs[0])
+    $(".song_name").innerHTML=`<h2>${data.songs[0].name}</h2>`;
+    $(".bg>img").src=`${data.songs[0].al.picUrl}`;
+    $(".disk_img>img").src=`${data.songs[0].al.picUrl}`
+})
+
+
+//获取歌词
+
+fetch(`http://localhost:3000/lyric?id=${id}`,{
+    method:"GET",
+    node:"cors"
+}).then(r=>r.json()).then(data=>{
+    console.log(data)
+    var lrc=data.lrc.lyric;
+    var lyric = new window.Lyric(lrc, function(obj){
+        console.log(obj.txt)
+        $(".song_lyric").innerHTML=`${obj.txt}`
+        
+    })
+    // 播放效果
+    lyric.togglePlay();
+    $(".disk").onclick=function(){
+        if($(".audio audio").paused){
+            $(".audio audio").play()
+            $(".disk").classList.add("disk_rotate")//动起来
+            lyric.togglePlay()
+        }else{
+            $(".audio audio").pause()
+            $(".disk").classList.remove("disk_rotate")//不动
+            lyric.togglePlay()
+        }
     }
-}
+})
